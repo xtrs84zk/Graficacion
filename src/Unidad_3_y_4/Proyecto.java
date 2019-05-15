a5bb8b599eec58cc7c612fbf4ce414af5d30f6b7
xtrs84zk <xtrs84zk@gmail.com>
2019-05-15 18:51:30 -0500

    Proyecto

---
+++ src/Unidad_3_y_4/Proyecto.java
@@ -0,0 +1,70 @@
+package Unidad_3_y_4;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
+import javafx.stage.Stage;
+
+public class Proyecto extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //color luna
+        PhongMaterial color = new PhongMaterial(Color.YELLOW);
+        color.setSpecularPower(128);
+
+        PhongMaterial mat = new PhongMaterial();
+        Image diffuseMap = new Image(this.getClass().getResourceAsStream("2.jpg"));
+        mat.setDiffuseMap(diffuseMap);
+        mat.setSpecularColor(Color.YELLOW);
+
+        //Cargando el fondo, yeah
+        Image img = new Image(this.getClass().getResourceAsStream("maxresdefault.jpg"));
+        ImagePattern pattern = new ImagePattern(img);
+
+
+        //tierra
+        Sphere tierra = new Sphere();
+        tierra.setRadius(50.0);
+        tierra.setTranslateX(150);
+        tierra.setTranslateY(100);
+        tierra.setMaterial(color);
+
+
+        Group root = new Group(tierra);
+
+        //Creating a scene object
+        Scene scene = new Scene(root, 1280, 768);
+        //rotateAroundYAxis(root).play();
+        //Setting camera
+        PerspectiveCamera camera = new PerspectiveCamera(false);
+        camera.setTranslateX(0);
+        camera.setTranslateY(0);
+        camera.setTranslateZ(-10);
+        scene.setCamera(camera);
+        scene.setFill(pattern);
+
+        //Setting title to the Stage
+        stage.setTitle("Proyecto 3D");
+
+        //Adding scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
+
+
+    }
+}
+// private RotateTransition rotateAroundYAxis(Node node) {
+//   RotateTransition rotat
